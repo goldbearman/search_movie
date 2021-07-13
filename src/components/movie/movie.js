af7eaabe59c12b23ecfad2834aa825a0fec67e56
fsabirov <sabirovfarit@gmail.com>
2021-07-13 18:04:34 +0300

    r

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -12,52 +12,58 @@ import {Row, Col, Slider} from 'antd';
 
 export default class Movie extends Component {
 
-  constructor(props) {
-    super(props);
-    this.updateMovie();
-  }
+    constructor(props) {
+        super(props);
+        this.updateMovie();
+    }
 
-  swapiService = new SwapiService();
+    swapiService = new SwapiService();
 
-  state = {
-    movie: {},
-  };
+    state = {
+        movie: {},
+    };
 
-  updateMovie() {
-    this.swapiService.getSearchMovie().then((item) => {
-      // console.log(item);
-      this.setState({
-        movie: item
-      });
-    })
-  }
+    updateMovie() {
+        this.swapiService.getSearchMovie().then((item) => {
+            // console.log(item);
+            this.setState({
+                movie: item
+            });
+        })
+    }
 
-  render() {
+    onChangeStars = (stars) => {
+        console.log(stars);
+        this.swapiService.postRate(stars, this.props.movie.id).then(result =>console.log(result))
+    }
 
-    const {movie:{vote_average, original_title, overview,poster_path}} = this.props
-    // console.log(this.props.movie)
+    render() {
 
-    return (
-      <Col span={12}>
-        <div className='card'>
-          <img className="card__poster" src={`https://image.tmdb.org/t/p/w185/${poster_path}`}
-               alt="Poster"/>
-          <div className="card__content">
-            <div className="rating">
-              <div>{vote_average}</div>
-            </div>
-            <h1>{original_title}</h1>
-            <div className="date">October 1, 2019</div>
-            <div className='genre'>
-              <button>War</button>
-            </div>
-            <p>
-              {overview}
-            </p>
-            <Rate className="stars" count="10"/>
-          </div>
-        </div>
-      </Col>
-    )
-  }
+        const {movie: {vote_average, original_title, overview, poster_path}} = this.props
+        console.log(this.props.movie.id)
+
+
+        return (
+            <Col span={12}>
+                <div className='card'>
+                    <img className="card__poster" src={`https://image.tmdb.org/t/p/w185/${poster_path}`}
+                         alt="Poster"/>
+                    <div className="card__content">
+                        <div className="rating">
+                            <div>{vote_average}</div>
+                        </div>
+                        <h1>{original_title}</h1>
+                        <div className="date">October 1, 2019</div>
+                        <div className='genre'>
+                            <button>War</button>
+                        </div>
+                        <p>
+                            {overview}
+                        </p>
+                        <Rate className="stars" count="10" onChange={this.onChangeStars}/>
+                    </div>
+                </div>
+            </Col>
+        )
+    }
 }
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,24 +1,56 @@
 export default class SwapiService {
 
-  _apiBase = 'https://api.themoviedb.org';
+    _apiBase = 'https://api.themoviedb.org';
+    _apiKey = '864cd2acdebdfd281550947ea6066439&query';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`)
+        }
+        return await res.json();
     }
-    return await res.json();
-  }
 
-  async getSearchMovies(query) {
-    const res = await this.getResource(`/3/search/movie?api_key=864cd2acdebdfd281550947ea6066439&query=${query}`);
-    return res.results;
-  }
+    async getSearchMovies(query) {
+        const res = await this.getResource(`/3/search/movie?api_key=864cd2acdebdfd281550947ea6066439&query=${query}`);
+        return res.results;
+    }
 
-  async getSearchMovie() {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=864cd2acdebdfd281550947ea6066439`);
-    const r = await res.json();
-    return r;
-  }
+    async getSearchMovie() {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=864cd2acdebdfd281550947ea6066439`);
+        const r = await res.json();
+        return r;
+    }
+
+    async getGuestSessionId() {
+        const guestSession = await this.getResource(`/3/authentication/guest_session/new?api_key=${this._apiKey}`);
+        console.log(guestSession)
+        const objRequest = await guestSession.guest_session_id;
+        console.log(objRequest)
+        return objRequest;
+    }
+
+    async postRate(stars, idMovie) {
+        console.log(stars,idMovie)
+        const guestSession = await this.getGuestSessionId();
+        console.log(guestSession)
+        // const res = await fetch(`${this._apiBase}/3/movie/${idMovie}/rating?api_key=${this._apiKey}&guest_session_id=${guestSession}`, {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/635744/rating?api_key=864cd2acdebdfd281550947ea6066439&guest_session_id=313ac9f5b38bf9ac4196e2e82f659c80`, {
+            method: 'POST',
+            body: {
+                value: stars
+            }
+        });
+        if (!res.ok) {
+            throw new Error(`Could not fetch , received ${res.status}`)
+        }
+
+        return await res.json().status_message;
+    }
+
+    async getRateMovie() {
+
+    }
 
 }
